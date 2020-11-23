a00075c1ff7e42e6ba0362f68b86764cddefdba6
jaeung <lju9176@naver.com>
2020-11-23 09:54:37 +0900

    첫 번째 commit

---
+++ week3/week3.ino
@@ -0,0 +1,16 @@
+int ledPin = 9; // ledPin을7번으로 정의
+
+
+void setup() {
+  pinMode(ledPin, OUTPUT);    //ledPin을 출력으로 설정
+}
+
+void loop() {
+  digitalWrite(ledPin, HIGH); //LED ON
+  delay(1000);                //1초 대기
+  digitalWrite(ledPin, LOW);  //LED OFF
+  delay(1000);                //1초 대기
+  digitalWrite(ledPin, LOW);  //LED OFF
+  delay(1000);                //1초 대기
+
+}
